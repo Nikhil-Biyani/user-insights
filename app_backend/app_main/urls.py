87d6f306e0c39daee115fe017d6d7f7d2b40c916
Safdar Faisal <safdar.faisal@gmail.com>
2024-07-09 22:37:55 +0530

    validation logic skeleton

--- app_backend/app_main/urls.py
+++ app_backend/app_main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import MovieDetailView, HomeView
+from .views import MovieDetailView, HomeView, register, login
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('movie/<int:pk>', MovieDetailView.as_view(), name="movie-detail"),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout')
 ]
--- app_backend/app_main/views.py
+++ app_backend/app_main/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import DetailView, ListView
 from .models import Movies
+from .forms import UserRegisterForm
+from django.contrib import auth
+from django.contrib.auth.forms import AuthenticationForm 
+
 
 class MovieDetailView(DetailView):
     model = Movies
@@ -22,3 +26,28 @@ class HomeView(ListView):
         context["cat_menu"] = cat_menu
 
         return context
+    
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth.login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form}) 
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form}) 
